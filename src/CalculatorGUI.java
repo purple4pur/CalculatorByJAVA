397721085b4a3e9d8fb8ee19facfa8cc1aecade1
yuhixyz <2539012056@qq.com>
2021-06-10 19:25:42 +0800

    更新取反和求补运算

--- src/CalculatorGUI.java
+++ src/CalculatorGUI.java
@@ -55,13 +55,13 @@ public class CalculatorGUI {
         displayBox = new JTextArea();
         displayBox.setPreferredSize(new Dimension(400, 50));
 
-        add = new CreateButton(" + ", new OperationButton(), bottomPane);
-        sub = new CreateButton(" - ", new OperationButton(), bottomPane);
-        and = new CreateButton(" & ", new OperationButton(), bottomPane);
-        or = new CreateButton(" | ", new OperationButton(), bottomPane);
-        xor = new CreateButton(" ^ ", new OperationButton(), bottomPane);
-        not = new CreateButton(" ~ ", new OperationButton(), bottomPane);
-        comp = new CreateButton(" # ", new OperationButton(), bottomPane); // # 表示求补码
+        add = new CreateButton("+", new OperationButton(), bottomPane);
+        sub = new CreateButton("-", new OperationButton(), bottomPane);
+        and = new CreateButton("&", new OperationButton(), bottomPane);
+        or = new CreateButton("|", new OperationButton(), bottomPane);
+        xor = new CreateButton("^", new OperationButton(), bottomPane);
+        not = new CreateButton("~", new EqualsButton(), bottomPane);  // ~表示求反码
+        comp = new CreateButton("#", new EqualsButton(), bottomPane); // # 表示求补码
         clear = new CreateButton("clear", new OperationButton(), bottomPane);
         one = new CreateButton("1", new NumberButton(), bottomPane);
         zero = new CreateButton("0", new NumberButton(), bottomPane);
